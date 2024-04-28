25a74eafeaa91ed160f2656fb0b1211bf6a8364c
Flavio Moreno <flaviomorenoz@gmail.com>
2024-04-28 11:57:27 -0500

    La primera vez

---
+++ agorero.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+	<h1>AVE AGORERA</h1>
+	<p>Las aves agoreras pueden marcar el camino al cambio.</p>
+
+</body>
+</html>
